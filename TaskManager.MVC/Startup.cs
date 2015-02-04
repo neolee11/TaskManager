180737e2cc0095d71239fad1ee2c3b8a3c6f743e
neolee11 <neolee11@yahoo.com>
2015-02-03 21:12:12 -0500

    Test

---
+++ TaskManager.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskManager.MVC.Startup))]
+namespace TaskManager.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
